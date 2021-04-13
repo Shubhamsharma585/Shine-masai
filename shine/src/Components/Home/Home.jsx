734e9c41e4e0e5bcb5e1a7b6f862449244296a58
ankitbajpai1607 <ankitbajpai1607@gmail.com>
2021-04-13 16:21:16 +0530

    HomePage 6%

--- shine/src/Components/Home/Home.jsx
+++ shine/src/Components/Home/Home.jsx
@@ -3,12 +3,17 @@ import styles from "./home.module.css";
 
 function Home() {
     return (
-        <div>
-            <div className={styles.background }>
+        <div className={styles.home }>
+            <div className={styles.a1background }>
                 <div>
                     <h1 className={styles.ah1 }>Growth on your mind?</h1>
                     <h2 className={styles.ah2 }>Explore 300,000+ jobs</h2>
                     <button className={styles.searchButton}>Job title,skills <img src="" alt="search"/></button>
+
+                </div>
+                <div className={styles.a2background}>
+                    <button className={styles.regButton}>Register for free</button>
+                    <div className={styles.signinp}><img src="" alt=""/><p>Sign in</p></div>
 
                 </div>
                 
