d63963bb9b709e9239d54b4a5d5ba0e90b1af223
ankitbajpai1607 <ankitbajpai1607@gmail.com>
2021-04-14 18:12:42 +0530

    HomePage--Pre3

--- shine/src/Components/Home/Premium.jsx
+++ shine/src/Components/Home/Premium.jsx
@@ -4,7 +4,7 @@ import styles from "./home.module.css";
 function Premium() {
     return (
         <div className={styles.Premium}>
-            <div style={{float:"left"}}>
+            <div style={{float:"left",marginTop:"35px"}}>
                 <div>
                     <div className={styles.crown}><div></div></div>
                     <div style={{color:"#ffb56a",fontSize:"30px",fontWeight:"600",lineHeight:"60px",float:"left"}}>Shine Premium</div>
@@ -14,14 +14,14 @@ function Premium() {
                 <div style={{fontSize:"28px",fontWeight:"600",padding:"10px 0 20px 0"}}>Fastrack your jon search with our premium services</div>
                 <div style={{clear:"both"}}></div>
                 <div>
-                    <div><div></div>Enhanced Profile</div>
-                    <div><div></div>10x higher visibility to recruters</div>
-                    <div><div></div>Insights on other applicants</div>
-                    <div><div></div>Interview Ready</div>
+                    <div style={{float:"left",fontsize:"14px",marginRight:"10px"}}><div className={styles.tick}></div><span>Enhanced Profile</span></div>
+                    <div style={{float:"left",fontsize:"14px",marginRight:"10px"}}><div className={styles.tick}></div><span>10x higher visibility to recruters</span></div>
+                    <div style={{float:"left",fontsize:"14px",marginRight:"10px"}}><div className={styles.tick}></div><span>Insights on other applicants</span></div>
+                    <div style={{float:"left",fontsize:"14px",marginRight:"10px"}}><div className={styles.tick}></div><span>Interview Ready</span></div>
                 </div>
-                <div></div>
-                <div>
-                    <button>Get Shine Premium</button>
+                <div style={{clear:"both"}}></div>
+                <div style={{float:"left",marginTop:"25px"}}>
+                    <button className={styles.PremiumButton}>Get Shine Premium</button>
                 </div>
 
             </div>
