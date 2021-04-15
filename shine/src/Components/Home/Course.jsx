001c28ff7f5f6f3a5035f7ff1c0a93d9b41286a6
ankitbajpai1607 <ankitbajpai1607@gmail.com>
2021-04-16 03:08:54 +0530

    HomePage----learning

--- shine/src/Components/Home/Course.jsx
+++ shine/src/Components/Home/Course.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from "./home.module.css";
 
 function Course() {
     return (
-        <div>
+        <div className={styles.course}>
+            <div className={styles.courselearning}></div>
+            <h2>Want to make your career future ready?</h2>
+            <h3>Upgrade your skills, give your career a boost</h3>
+
+            <button>Explore courses</button>
             
         </div>
     )
