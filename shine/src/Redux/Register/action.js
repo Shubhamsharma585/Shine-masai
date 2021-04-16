9880f3059da2ebec231ff03b0f1499365ef5f046
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-04-16 09:32:54 +0530

    ragistration and login done

--- shine/src/Redux/Register/action.js
+++ shine/src/Redux/Register/action.js
@@ -5,7 +5,69 @@ import Axios from "axios"
 
 
 
+export const registering = (payload) => dispatch => {
+    console.log("registering")
+    console.log(payload)
+    const {name, email, mobile, pass, gender, location, degree,
+        college, year, course, skills, title, 
+        company, industry, functionalarea } = payload
 
+    dispatch(registerrequest())
+    Axios.post("https://json-heroku-shubham.herokuapp.com/applicant",{
+        "name": name,
+    "title": "",
+    "email": email,
+    "mobile": mobile,
+    "resume": "",
+    "personal": {
+      "name": name,
+      "email": email,
+      "mobile": mobile,
+      "dob": "",
+      "location": location,
+      "gender": gender
+    },
+    "worksummary": {
+      "title": title,
+      "summary": "",
+      "experience": [ ],
+      "largestteam": "",
+      "anualsalary": "",
+      "notice": ""
+    },
+    "employment": {
+      "title": title,
+      "company": company,
+      "duration": "",
+      "industry": industry,
+      "functionalarea": functionalarea
+    },
+    "education": [
+      {
+        "title": degree,
+        "branch": course,
+        "college": college
+      }
+    ],
+    "skills": skills,
+    "desired": {
+      "role": "",
+      "location": "",
+      "department": "",
+      "industry": "",
+      "jobtype": "",
+      "shifttype": "",
+      "salary": ""
+    }
+
+
+    })
+    .then((res) => {
+        console.log(res.data)
+        dispatch(registersuccess(res.data))
+    })
+
+}  
 
 
 
--- shine/src/Routes/Routes.jsx
+++ shine/src/Routes/Routes.jsx
@@ -3,27 +3,15 @@ import { Route, Switch } from "react-router-dom"
 import Login from "../Components/LogIn/Login"
 import Register from "../Components/Register/Register";
 import Home from "../Components/Home/Home"
-<<<<<<< HEAD
-import NavBar from "../Components/Navbar/NavBar";
-import { JobDescription } from "../Components/JobDescription/JobDescription";
-
-=======
 import { JobDescription } from "../Components/JobDescription/JobDescription";
 import NavBar from "../Components/Navbar/NavBar";
 import JobAlert from "../Components/CreateJobAlert/JobAlert";
->>>>>>> 52ab4eb1f6ee4e4865e9c92233ecf30138ffb846
 
 
 
 
 function Routes()
 {
-<<<<<<< HEAD
-   return(
-      <div>
-         <NavBar />
-         <Switch>
-=======
 
 
 
@@ -34,36 +22,26 @@ function Routes()
         <div>
       <NavBar />
      <Switch>
->>>>>>> 52ab4eb1f6ee4e4865e9c92233ecf30138ffb846
             <Route path="/" exact>
                <Home/>
             </Route>
             <Route path="/myshine/login" exact>
                <Login/>
             </Route>
-<<<<<<< HEAD
-=======
             <Route path="/jobalert" exact>
                <JobAlert />
             </Route>
->>>>>>> 52ab4eb1f6ee4e4865e9c92233ecf30138ffb846
-            <Route path="/registration/parser/" exact>
+            <Route path="/registration" exact>
                <Register/>
             </Route>
             <Route path="/jobdescription" exact>
                <JobDescription />
             </Route>
-<<<<<<< HEAD
-         </Switch>
-      </div>
-   )
-=======
        </Switch>
 
 
         </div>
     )
->>>>>>> 52ab4eb1f6ee4e4865e9c92233ecf30138ffb846
 }
 
 export default Routes;
