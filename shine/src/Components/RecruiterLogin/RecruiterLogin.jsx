e82aa623aba21a9d5ef7c5872687e9ec7be41eeb
Vinesh3124 <vinesh.nair31@gmail.com>
2021-04-18 18:35:49 +0530

    Fixed css issues

--- shine/src/Components/RecruiterLogin/RecruiterLogin.jsx
+++ shine/src/Components/RecruiterLogin/RecruiterLogin.jsx
@@ -33,7 +33,7 @@ function RecruiterLogin() {
         <div className={styles.cont1loginrecruiter}>
           <h2>Recruiter Login</h2>
           <input type="text" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br/><br/>
-          <input type="text" value={pass}  placeholder="Password" onChange={(e) => setPass(e.target.value)}/><br/><br/>
+          <input type="password" value={pass}  placeholder="Password" onChange={(e) => setPass(e.target.value)}/><br/><br/>
           <div onClick={handleLogin} className={styles.login}>Login</div>
           <p style={{fontSize:"10px", color:"rgb(112, 82, 82)"}}>By syncing your social media account, you agree to shine <span style={{color:"blue"}}>terms and conditions</span></p>
         </div>
